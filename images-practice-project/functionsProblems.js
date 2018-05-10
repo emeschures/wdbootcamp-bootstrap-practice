076fe6964a38cd898490970d38a075932e020718
Eli Meschures <meschures.eli@gmail.com>
2018-05-10 11:29:27 -0700

    forLoopsProblems and functionProblems

---
+++ images-practice-project/functionsProblems.js
@@ -0,0 +1,28 @@
+function isEven(num) {
+  if(num % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// function factorial(num) {
+//   if(num < 0){
+//     return -1;
+//   } else if(num === 0) {
+//     return 1;
+//   } else {
+//     return (num * factorial(num - 1));
+//   }
+// }
+
+function factorial(num) {
+  var result = 1;
+  for(var i = 0; i <= num; i++) {
+    result *= 1;
+  }
+}
+
+function kebabToSnake(str) {
+  var newStr = str.replace(/-/g, "_");
+}
